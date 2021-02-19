e7e8f76043fe0863b0ef56c834f41a51b05e0503
beatriz <biia.perez10@gmail.com>
2021-02-18 23:56:33 -0300

    :package: Adicionando os post

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,16 +5,31 @@ const Home  = () => {
     return (
         <div className="header">
             <img className="logo" src="/logo.svg"></img> 
-            <div>
-                <div> 
+            <h2> Feed </h2>
+            <div className="line"></div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
                     
-                </div>
-                <div>
-                    <h1> Titulo Post</h1>
-                </div>
-            </div>
         </div>
     )
 } 
+
+const Card =() =>{
+    return(
+        <div className="home-container">
+                            <div className="home-card">
+                                <img alt="url" src="https://i.ytimg.com/vi/5qap5aO4i9A/hq720_live.jpg?sqp=CMCkvIEG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzBk27_ibBK-HO-sOKXWsoKl-OJg"></img>
+
+                                <div className="title-container">
+                                    <img className="avatar" alt="avatar" src="https://yt3.ggpht.com/ytc/AAUvwnht6c9eAFl7xdGc798TcXnHz0xZhwM9tmQ99AqO1g=s68-c-k-c0x00ffffff-no-rj"></img>
+                                </div>
+                                <h3>Titulo post</h3>
+                            </div>
+                    </div>
+    )
+}
 
 export default Home
