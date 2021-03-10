ffe6b518f17ebfdcbef03000b341c22a754a60d7
beatriz <biia.perez10@gmail.com>
2021-03-09 21:24:54 -0300

    :up - atualização no layout de cadastro

--- src/Pages/form/index.js
+++ src/Pages/form/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
-const index = () => {
+
+const DEFAULT_LIST = {
+    title: '',
+    imageurl: '',
+    userAvatar: '',
+    description: '',
+    date: (new Date().toDateString().substr(0,10))
+
+}
+
+const Index = () => {
+
+    const [values, setValues] = useState(DEFAULT_LIST);
+
+    const handlerChange = (evento) =>{
+        setValues(values =>({...values, title: evento.target.value}))
+    }
+
     return (
         <div className="form-container">
             <div className="description-line">
@@ -8,12 +25,49 @@ const index = () => {
                 <div className="line"></div>
             </div>
             <form>
-                    <label>Titulo do post:</label>
-                    <input></input>
+                  <FormGroup
+                    required
+                    name="title"
+                    label="Titulo"
+                    onChange={handlerChange}
+                    value={values.title}
+                />
+                <FormGroup
+                    required
+                    name="description"
+                    label="Descrição"
+                    onChange={handlerChange}
+                    value={values.description}
+                />
+                <FormGroup
+                    required
+                    name="date"
+                    label="Data"
+                    onChange={handlerChange}
+                    value={values.date}
+                    type="date"
+                />
+                <FormGroup
+                    required
+                    name="imageUrl"
+                    label="Imagem"
+                    onChange={handlerChange}
+                    value={values.imageUrl}
+                />
+                <button type="submit">Enviar</button>
             </form>
         </div>
     )
 }
 
-export default index 
+const FormGroup = ({value, name, onCharge, label}) => {
+    return(
+        <div className="form-group">
+            <label htmlFor={name}>{label}:</label>
+            <input name={name} value={value} onCharge={onCharge}></input>
+        </div>
+    )
+}
+
+export default Index 
 
